9e89507ba0c8ddc3bbd363f3ff8e68344c761ff8
Conor Erickson <89137464+conorerickson@users.noreply.github.com>
2022-06-25 14:10:47 -0400

    Added Widgets component, styled search input

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
+import Widgets from '../components/Widgets'
 
 export default function Home() {
   return (
@@ -20,7 +21,7 @@ export default function Home() {
       <Feed />
 
       {/* Widgets */}
-
+      <Widgets />
       
       {/* Modal */}
 
