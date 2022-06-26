f91ee86ad64cd04726a458e4aff7638c2a5be693
Conor Erickson <89137464+conorerickson@users.noreply.github.com>
2022-06-26 14:50:41 -0400

    Added 'Users to follow' section

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
 import Widgets from '../components/Widgets'
 
-export default function Home({newsResults}) {
+export default function Home({newsResults, randomUsersResults}) {
   return (
     <div>
       <Head>
@@ -21,7 +21,7 @@ export default function Home({newsResults}) {
       <Feed />
 
       {/* Widgets */}
-        <Widgets newsResults={newsResults.articles} />
+        <Widgets newsResults={newsResults.articles} randomUsersResults={randomUsersResults.results} />
       
       {/* Modal */}
 
@@ -32,14 +32,19 @@ export default function Home({newsResults}) {
 }
 
 
-//API: https://saurav.tech/NewsAPI/top-headlines/category/business/us.json
-
 export async function getServerSideProps() {
   const newsResults = await fetch("https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
   ).then((res) => res.json());
+
+  //Who to follow section
+
+  const randomUsersResults = await fetch("https://randomuser.me/api/?results=30&inc=name,login,picture").then((res) => res.json());
+
   return {
     props: {
       newsResults,
+      randomUsersResults
     }
   }
 }
+
